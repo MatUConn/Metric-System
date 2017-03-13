f1a50c3e26cd34ba5b13b43d097a4616bf3e1160
Mat <mfox@energyresourcesusa.net>
2017-03-13 16:01:44 -0400

    Add files via upload

---
+++ includes/login_functions.inc.php
@@ -0,0 +1,66 @@
+﻿<?php
+# JK SOW p1.0
+# login_functions.inc.php
+# Author: JJK
+# Create Date: 10/01/2013
+# Description: Implements Login functionality of SOW system via function definitions
+# Revision History:
+
+// #Function redirect_user
+// #Parameters: page (default=index.php)
+// #Return Type: N/A, no return
+function redirect_user($page = 'index.php') {
+	//absolute URL for website
+	$url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
+	
+	//remove trailing slashes (either '/' or '\') from right side of url
+	$url = rtrim($url, '/\\');
+	
+	//append slashes and page via concatention assignment operator
+	$url .= '/' . $page;
+	echo $url;
+	
+	//redirect user
+	header("Location: $url");
+	exit(); //quit script
+	
+} //end of redirect_user function
+
+// #Function check_login
+// #Parameters: dbc, user_name (default is empty string), password (default is empty string)
+// #Return Type: array - contains boolean of result and error array if false or mysqli assoc array if true
+// Fucntion validates login credentials
+function check_login($dbc, $user_name='', $password='') {
+	$errors = array();
+	if (empty($user_name)) {
+		$errors[] = 'User name is required.';
+	}
+	else {
+		$u_n = mysqli_real_escape_string($dbc, trim($user_name));
+	}
+	if (empty($password)) {
+		$errors[] = 'Password is required.';
+	}
+	else {
+		$p_w = mysqli_real_escape_string($dbc, trim($password));
+	}
+	
+	//if no errors, run database query
+	if (empty($errors)) {
+		$q_chk_creds = "SELECT user_id, user_name
+										FROM user
+										WHERE user_name = '$u_n' AND password = UNHEX(SHA1('$p_w'))";
+		
+		$r_chk_creds = @mysqli_query($dbc, $q_chk_creds);
+		if (mysqli_num_rows($r_chk_creds) == 1) {
+			$row_creds = mysqli_fetch_array($r_chk_creds, MYSQLI_ASSOC);
+			//return array of sucessful login(TRUE) and data in result set, function will stop here if condition is true
+			return array(TRUE, $row_creds);
+		}
+		else { //no match
+			$errors[] = 'Match for given user name and password not found';
+		}
+	} // end of if empty errors
+	// return errors that were generated
+	return array(FALSE, $errors);
+} //end of check_login function
---
+++ includes/login_page.inc.php
@@ -0,0 +1,32 @@
+﻿<?php
+# JK SOW p1.0
+# login.inc.php
+# Author: JJK
+# Create Date: 10/01/2013
+# Description: Implements Login functionality of SOW system
+# Revision History:
+
+$page_title = 'Login';
+include('includes/header.html');
+
+//Print error messages if they exist
+
+if (isset($errors) && !empty($errors)) {
+	echo '<h1> Error </h1>
+				<p class="error">The following error(s) occurred:<br />';
+	//print each message
+	foreach($errors as $msg) {
+		echo " - $msg<br />\n";
+	}
+	echo '<p>Please try again.</p>';
+}
+
+//Display login form
+?>
+<form role="form" action="login.php" method="post">
+	<input type="text" class="form-control" name="user_name" placeholder="User Name" />
+	<input type="password" class="form-control" name="password" placeholder="Password" />
+	<button type="submit" class="btn btn-default">Sign In</button>
+</form>
+
+<?php include('includes/footer.html');
