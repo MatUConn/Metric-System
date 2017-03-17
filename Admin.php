1dd082c9a08725deeb6f7df2cca66e2a5fdfe376
Mat <mfox@energyresourcesusa.net>
2017-03-17 16:44:33 -0400

    Added "Team Member Activity" widget
    Also removed other template widgets.

--- Admin.php
+++ Admin.php
@@ -47,107 +47,18 @@ $Username = $UserInfo[0] . ' ' . $UserInfo[1];
           <div class="">
 
             <div class="row">
-              <div class="col-md-4 col-sm-6 col-xs-12">
-                <div class="x_panel fixed_height_320">
-                  <div class="x_title">
-                    <h2>App Devices <small>Sessions</small></h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                    <h4>App Versions</h4>
-                    <div class="widget_summary">
-                      <div class="w_left w_25">
-                        <span>1.5.2</span>
-                      </div>
-                      <div class="w_center w_55">
-                        <div class="progress">
-                          <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 66%;">
-                            <span class="sr-only">60% Complete</span>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="w_right w_20">
-                        <span>123k</span>
-                      </div>
-                      <div class="clearfix"></div>
-                    </div>
-
-                    <div class="widget_summary">
-                      <div class="w_left w_25">
-                        <span>1.5.3</span>
-                      </div>
-                      <div class="w_center w_55">
-                        <div class="progress">
-                          <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 45%;">
-                            <span class="sr-only">60% Complete</span>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="w_right w_20">
-                        <span>53k</span>
-                      </div>
-                      <div class="clearfix"></div>
-                    </div>
-                    <div class="widget_summary">
-                      <div class="w_left w_25">
-                        <span>1.5.4</span>
-                      </div>
-                      <div class="w_center w_55">
-                        <div class="progress">
-                          <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
-                            <span class="sr-only">60% Complete</span>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="w_right w_20">
-                        <span>23k</span>
-                      </div>
-                      <div class="clearfix"></div>
-                    </div>
-                    <div class="widget_summary">
-                      <div class="w_left w_25">
-                        <span>1.5.5</span>
-                      </div>
-                      <div class="w_center w_55">
-                        <div class="progress">
-                          <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 5%;">
-                            <span class="sr-only">60% Complete</span>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="w_right w_20">
-                        <span>3k</span>
-                      </div>
-                      <div class="clearfix"></div>
-                    </div>
-                    <div class="widget_summary">
-                      <div class="w_left w_25">
-                        <span>0.1.5.6</span>
-                      </div>
-                      <div class="w_center w_55">
-                        <div class="progress">
-                          <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 2%;">
-                            <span class="sr-only">60% Complete</span>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="w_right w_20">
-                        <span>1k</span>
-                      </div>
-                      <div class="clearfix"></div>
-                    </div>
-
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-4 col-sm-6 col-xs-12">
+			
+			  <!-- Start Team Member Activity -->
+			  <?php
+			  $Q_Get_TMembers = "SELECT pm.User_ID, COUNT(pm.User_ID) AS Amount, CONCAT(u.first_name, ' ', SUBSTRING(u.last_name,1,1)) AS User
+								FROM ProjectMembers pm
+								LEFT JOIN user u ON (pm.User_ID = u.user_id)
+								GROUP BY pm.User_ID 
+								ORDER BY Amount DESC
+								LIMIT 5";
+			  $R_Get_TMembers = $mysqli->query($Q_Get_TMembers);
+			  ?>
+              <div class="col-md-6 col-sm-6 col-xs-12">
                 <div class="x_panel fixed_height_320">
                   <div class="x_title">
                     <h2>Team Member Activity</h2>
@@ -167,49 +78,37 @@ $Username = $UserInfo[0] . ' ' . $UserInfo[1];
                       </th>
                       <th>
                         <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
-                          <p class="">Device</p>
+                          <p class="">User</p>
                         </div>
                         <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-                          <p class="">Progress</p>
+                          <p class="">Count</p>
                         </div>
                       </th>
                     </tr>
                     <tr>
                       <td>
-                        <canvas id="canvas1" height="140" width="140" style="margin: 15px 10px 10px 0"></canvas>
+                        <canvas id="canvas1" height="200" width="200" style="margin: 15px 10px 10px 0"></canvas>
                       </td>
                       <td>
                         <table class="tile_info">
+						<?php
+						$Colors = array("blue", "green", "purple", "red", "aero");
+						$i = 0;
+						while($TeamMembers = $R_Get_TMembers->fetch_array(MYSQLI_ASSOC)){
+						  echo'
                           <tr>
                             <td>
-                              <p><i class="fa fa-square blue"></i>IOS </p>
+                              <p><i class="fa fa-square '.$Colors[$i].'"></i>'.$TeamMembers['User'].'</p>
                             </td>
-                            <td>30%</td>
+                            <td>'.$TeamMembers['Amount'].'</td>
                           </tr>
-                          <tr>
-                            <td>
-                              <p><i class="fa fa-square green"></i>Android </p>
-                            </td>
-                            <td>10%</td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <p><i class="fa fa-square purple"></i>Blackberry </p>
-                            </td>
-                            <td>20%</td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <p><i class="fa fa-square aero"></i>Symbian </p>
-                            </td>
-                            <td>15%</td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <p><i class="fa fa-square red"></i>Others </p>
-                            </td>
-                            <td>30%</td>
-                          </tr>
+						  ';
+						  //Making arrays to reference in scripts below
+						  $MembersCountArray[] = $TeamMembers['Amount'];
+						  $MembersArray[] = $TeamMembers['User'];
+						  $i++;
+						}
+						?>
                         </table>
                       </td>
                     </tr>
@@ -217,123 +116,7 @@ $Username = $UserInfo[0] . ' ' . $UserInfo[1];
                   </div>
                 </div>
               </div>
-
-              <div class="col-md-4 col-sm-6 col-xs-12">
-                <div class="x_panel fixed_height_320">
-                  <div class="x_title">
-                    <h2>Profile Settings <small>Sessions</small></h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                    <div class="dashboard-widget-content">
-                      <ul class="quick-list">
-                        <li><i class="fa fa-line-chart"></i><a href="#">Achievements</a></li>
-                        <li><i class="fa fa-thumbs-up"></i><a href="#">Favorites</a></li>
-                        <li><i class="fa fa-calendar-o"></i><a href="#">Activities</a></li>
-                        <li><i class="fa fa-cog"></i><a href="#">Settings</a></li>
-                        <li><i class="fa fa-area-chart"></i><a href="#">Logout</a></li>
-                      </ul>
-
-                      <div class="sidebar-widget">
-                        <h4>Profile Completion</h4>
-                        <canvas width="150" height="80" id="foo" class="" style="width: 160px; height: 100px;"></canvas>
-                        <div class="goal-wrapper">
-                          <span id="gauge-text" class="gauge-value pull-left">0</span>
-                          <span class="gauge-value pull-left">%</span>
-                          <span id="goal-text" class="goal-value pull-right">100%</span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-6 col-sm-6 col-xs-12 widget_tally_box">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>User Uptake</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-
-                    <div id="graph_bar" style="width:100%; height:200px;"></div>
-
-                    <div class="col-xs-12 bg-white progress_summary">
-
-                      <div class="row">
-                        <div class="progress_title">
-                          <span class="left">Escudor Wireless 1.0</span>
-                          <span class="right">This sis</span>
-                          <div class="clearfix"></div>
-                        </div>
-
-                        <div class="col-xs-2">
-                          <span>SSD</span>
-                        </div>
-                        <div class="col-xs-8">
-                          <div class="progress progress_sm">
-                            <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="89"></div>
-                          </div>
-                        </div>
-                        <div class="col-xs-2 more_info">
-                          <span>89%</span>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="progress_title">
-                          <span class="left">Mobile Access</span>
-                          <span class="right">Smart Phone</span>
-                          <div class="clearfix"></div>
-                        </div>
-
-                        <div class="col-xs-2">
-                          <span>App</span>
-                        </div>
-                        <div class="col-xs-8">
-                          <div class="progress progress_sm">
-                            <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="79"></div>
-                          </div>
-                        </div>
-                        <div class="col-xs-2 more_info">
-                          <span>79%</span>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="progress_title">
-                          <span class="left">WAN access users</span>
-                          <span class="right">Total 69%</span>
-                          <div class="clearfix"></div>
-                        </div>
-
-                        <div class="col-xs-2">
-                          <span>Usr</span>
-                        </div>
-                        <div class="col-xs-8">
-                          <div class="progress progress_sm">
-                            <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="69"></div>
-                          </div>
-                        </div>
-                        <div class="col-xs-2 more_info">
-                          <span>69%</span>
-                        </div>
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-              </div>
+			  <!-- End Team Member Activity -->
 
               <!-- Start users list -->
 			  <?
@@ -527,28 +310,22 @@ $Username = $UserInfo[0] . ' ' . $UserInfo[1];
           type: 'doughnut',
           tooltipFillColor: "rgba(51, 51, 51, 0.55)",
           data: {
-            labels: [
-              "Symbian",
-              "Blackberry",
-              "Other",
-              "Android",
-              "IOS"
-            ],
+            labels: <?php echo json_encode($MembersArray); ?>,
             datasets: [{
-              data: [15, 20, 30, 10, 30],
+              data: <?php echo json_encode($MembersCountArray); ?>,
               backgroundColor: [
-                "#BDC3C7",
+                "#3498DB",
+				"#26B99A",
                 "#9B59B6",
                 "#E74C3C",
-                "#26B99A",
-                "#3498DB"
+				"#BDC3C7"
               ],
               hoverBackgroundColor: [
-                "#CFD4D8",
+                "#49A9EA",
+				"#36CAAB",
                 "#B370CF",
                 "#E95E4F",
-                "#36CAAB",
-                "#49A9EA"
+				"#CFD4D8"
               ]
             }]
           },
